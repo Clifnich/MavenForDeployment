327febcf222b4a5db63a9c448b5a4fd5b837693b
Qian <puqian@ebay.com>
2017-06-19 14:59:31 +0800

    Maven Project For Deployment

---
+++ src/main/java/Puzhen/com/MavenForDeploy/Adder.java
@@ -0,0 +1,8 @@
+package Puzhen.com.MavenForDeploy;
+
+public class Adder {
+
+	public int add(int x1, int x2) {
+		return x1 + x2;
+	}
+}
---
+++ src/main/java/Puzhen/com/MavenForDeploy/App.java
@@ -0,0 +1,13 @@
+package Puzhen.com.MavenForDeploy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
